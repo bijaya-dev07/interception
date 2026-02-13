92110fb5e23d844b19ebf04db59738b0afba9005
kojing777 <kojingmoktan92@gmail.com>
2026-02-13 22:44:54 +0545

    Refactor Navbar component to use Link for navigation and add Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <div>
+    <h1>Contact Page</h1>
+  </div>;
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,19 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="w-full h-16 flex items-center justify-between px-4">
-      <div className="text-lg font-bold">Bijaya Tamang</div>
+      <div className="text-lg nav-logo font-bold">Bijaya Tamang</div>
       <div className="space-x-4">
-        <a href="#about" className="text-gray-600 hover:text-gray-900">
+        <Link href="#about" className="text-gray-600 hover:text-gray-900">
           About
-        </a>
-        <a href="#projects" className="text-gray-600 hover:text-gray-900">
+        </Link>
+        <Link href="#projects" className="text-gray-600 hover:text-gray-900">
           Projects
-        </a>
-        <a href="#contact" className="text-gray-600 hover:text-gray-900">
+        </Link>
+        <Link href="/contact" className="text-gray-600 hover:text-gray-900">
           Contact
-        </a>
+        </Link>
       </div>
     </nav>
   );
